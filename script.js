37e70b55ac595e42fb35f0c5814faebda1636158
Chuka Ukekwe <ukekwe.chuka@gmail.com>
2021-07-14 22:22:30 -0400

    added first script

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('This will be an epic undergoing!');
+
+const clearInput = document.getElementById('clearButton');
+const phraseInput = document.getElementById('inputPhrase');
+
+clearInput.addEventListener('click', () => {
+    phraseInput.value = "";
+    console.log('Search bar text has been emptied.')
+});
