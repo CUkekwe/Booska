649b00eb75c3a307a0be74e259578d72edb3d24c
Chuka Ukekwe <ukekwe.chuka@gmail.com>
2021-07-24 09:16:22 -0400

    messing around with the javascript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-console.log('This will be an epic undergoing!');
+const Reverso = require('reverso-api');
+const reverso = new Reverso();
 
 const clearInput = document.getElementById('clearButton');
 const phraseInput = document.getElementById('inputPhrase');
@@ -7,3 +8,17 @@ clearInput.addEventListener('click', () => {
     phraseInput.value = "";
     console.log('Search bar text has been emptied.')
 });
+
+const search = document.getElementById('search');
+const inputLang = document.getElementById("inputLang");
+const outputLang = document.getElementById("outputLang");
+
+let searchProtocol = function () {
+    if (inputLang.value == outputLang.value) {
+        alert(`You have ${inputLang.options[inputLang.selectedIndex].text} selected in both options! Please select two different languages.`)
+    }
+};
+
+search.addEventListener('click', searchProtocol);
+
+console.log(inputLang.options[inputLang.selectedIndex].text);
